4254323030146ea42c09d193a1434781ab50a06e
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-02 00:36:23 +0530

    After the interview and evaluation.

--- client/src/pages/Bmi.jsx
+++ client/src/pages/Bmi.jsx
@@ -1,64 +1,61 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 export const Bmi = () => {
-  const [height, setHeight] = useState();
-  const [weight, setWeight] = useState();
-  // const [render, setRender] = useState(true);
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
   const [bmi, setBmi] = useState();
-  const [result, setResult] = useState("");
+  let result = "";
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(typeof height, weight);
-    // setRender(!render);
-    // getBmi({ height, weight })
-    calculateBMI()
-    setHeight(0);
-    setWeight(0);
+    calculateBMI({ height, weight });
+    setHeight("");
+    setWeight("");
   };
-  
-  const calculateBMI=async()=>{
-    if(!weight || !height || isNaN(height) || isNaN(weight)){
-      setResult("")
-      setBmi("")
-      return
+
+  const calculateBMI = async (obj) => {
+    if (!weight || !height || isNaN(height) || isNaN(weight)) {
+      result = "";
+      setBmi("");
+      return;
     }
     try {
-      const res=await axios.post("https://bmi-9fe8.onrender.com/calculateBMI",{height,weight})
-      const data =res.data
-      setBmi(data)
+      const res = await axios.post(
+        "https://bmi-9fe8.onrender.com/calculateBMI",
+        obj
+      );
+      const data = res.data.bmi;
+      setBmi(data);
     } catch (err) {
       console.log(err);
     }
-  }
-  // useEffect(() => {
-    
-  // }, [height, weight]);
-
-  const getBmi = (obj) => {
-    axios
-      .post("https://bmi-9fe8.onrender.com/calculateBMI", obj)
-      .then((res) => {
-        setBmi(res.data);
-        console.log(res.data);
-      })
-      .catch((err) => console.log(err));
   };
 
-  // if (bmi < 18.5) {
-  //   setResult("Underweight");
-  // } else if (bmi >= 18.5 && bmi < 24.9) {
-  //   setResult("Normal weight");
-  // } else if (bmi >= 24.9 && bmi < 29.9) {
-  //   setResult("Overweight");
-  // } else {
-  //   setResult("Obese");
-  // }
+  if (bmi < 18.5) {
+    result = "Underweight";
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    result = "Normal weight";
+  } else if (bmi >= 24.9 && bmi < 29.9) {
+    result = "Overweight";
+  } else if (bmi >= 29.9) {
+    result = "Obese";
+  }
 
   return (
-    <>
-      <form action="" onSubmit={handleSubmit}>
+    <div style={{ textAlign: "center", padding: "100px", fontWeight: "bold" }}>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          alignContent: "center",
+          gap: "10px",
+        }}
+      >
         <input
           type="number"
           placeholder="Height in meter"
@@ -73,8 +70,8 @@ export const Bmi = () => {
         />
         <button type="submit">Calculate</button>
       </form>
-      <div>BMI: {bmi}</div>
-      <div>Result: {result}</div>
-    </>
+      <div style={{ color: "red", marginTop: "5px" }}>BMI: {bmi}</div>
+      <div style={{ color: "red", marginTop: "5px" }}>Result: {result}</div>
+    </div>
   );
 };
